29187c92a3e6a9824788e65b33ebfa01e5d42d34
Chris Praktish & parker smathers & Susan Yi <pair+cprakti.psmathers.yisusans@devbootcamp.com>
2016-06-01 18:29:12 -0400

    create answers_controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,7 @@ post '/questions/:id/votes' do
   question = Question.find_by(params[:id])
   vote = Vote.new(voteable_id: question.id, user_id: current_user.id, voteable_type: "Question" )
   if vote.save?
-    erb :"votes/_question_votes"
+    erb :"questions/show"
   end
 end
 
@@ -11,7 +11,7 @@ post '/answers/:id/votes' do
   vote = Vote.new(voteable_type: "Answer", voteable_id: answer.id, user_id: current_user.id)
 
   if vote.save?
-    erb :"votes/_answer_votes"
+    erb :"questions/show"
   end
 
 end
