8723764f8969413b3a1380459a5cb973289aedb6
Chris Praktish <chris.praktish@gmail.com>
2016-06-01 16:42:31 -0400

    Add some questions routes and views

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,25 @@
+get '/questions' do
+  @questions = Question.all
+  erb :'questions/index'
+end
+
+get '/questions/new' do
+  erb :'questions/new'
+end
+
+post '/questions' do
+  @author = User.find_by(id: params['user_id'])
+  @question = Question.new(title: params['title'], body: params['body'], username: @author)
+  if Question.save
+    redirect '/questions'
+  else
+    @errors = @question.errors.full_messages
+    erb :'questions/new'
+  end
+end
+
+get '/questions/:id' do
+  @question = Question.find_by(id: params['id'])
+  erb :'questions/show'
+end
+
