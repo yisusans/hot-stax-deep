dac4e6ad56b6be233ed2db8d7308b0b8388af9bc
Chris Praktish & parker smathers & Susan Yi <pair+cprakti.psmathers.yisusans@devbootcamp.com>
2016-06-02 19:40:23 -0400

    send an error message when you type in nothing

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,11 +3,11 @@ post '/questions/:id/answers' do
   answer = Answer.new(user_id: current_user.id, question_id: @question.id, answer: params[:answer])
   @comments = @question.comments
   @answers = @question.answers
-  binding.pry
+
   if answer.save
     erb :'answers/_all_answers', layout: false
   else
-    @error = "Please type an answer"
+    @errors = "Please type an answer:"
     erb :'answers/_all_answers', layout: false
   end
 
