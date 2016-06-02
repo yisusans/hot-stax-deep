04497f9cb62fca3c6bcd8b7a880690236380c4d6
Chris Praktish & parker smathers & Susan Yi <pair+cprakti.psmathers.yisusans@devbootcamp.com>
2016-06-02 11:17:08 -0400

    add some psuedo-code

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,10 @@
 post '/questions/:id/votes' do
   question = Question.find_by(params[:id])
   vote = Vote.new(voteable_id: question.id, user_id: current_user.id, voteable_type: "Question" )
+  # add logic to make sure person can only vote once
+  # already_voted? = Vote.find_by( voteable_id: question.id, user_id: current_user.id)
+  # create a method that validates in the vote class
+  # throw an error - try to do it in js
   if vote.save?
     erb :"questions/show"
   end
