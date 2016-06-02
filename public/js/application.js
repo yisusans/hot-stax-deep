dd9e313379c5250ba2d38351c688a6ed820bcbe5
Chris Praktish & parker smathers & Susan Yi <pair+cprakti.psmathers.yisusans@devbootcamp.com>
2016-06-02 15:01:51 -0400

    hash out js file

--- public/js/application.js
+++ public/js/application.js
@@ -1,34 +1,34 @@
-$(document).ready(function () {
-  $('form').on('submit', function(event) {
-    event.preventDefault();
+// $(document).ready(function () {
+//   $('form').on('submit', function(event) {
+//     event.preventDefault();
 
-    $target = $(event.target)
+//     $target = $(event.target)
 
 
-    var reqArgs = {
-      url: $target.attr('action'),
-      method: $target.attr('method'),
-      data: $target.serialize(),
-      dataType: 'html'
-    };
+//     var reqArgs = {
+//       url: $target.attr('action'),
+//       method: $target.attr('method'),
+//       data: $target.serialize(),
+//       dataType: 'html'
+//     };
 
-    var reqObj = $.ajax(reqArgs)
+//     var reqObj = $.ajax(reqArgs)
 
-    reqObj.done(function(response){
-      $('#die-container').empty();
-      $('#die-container').append(response)
-    });
+//     reqObj.done(function(response){
+//       $('#die-container').empty();
+//       $('#die-container').append(response)
+//     });
 
-    reqObj.fail(function(response){
-      console.log("I failed you.")
+//     reqObj.fail(function(response){
+//       console.log("I failed you.")
 
-    });
+//     });
 
-  });
-  // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
-  //   3- use jQuery to submit an AJAX post to the form's action
-  //   4- when the AJAX post is done, display the new die roll using jQuery
+//   });
+//   // PSEUDO-CODE:
+//   //   1- intercept the form submission event using jQuery
+//   //   2- prevent the default action for that event from happening
+//   //   3- use jQuery to submit an AJAX post to the form's action
+//   //   4- when the AJAX post is done, display the new die roll using jQuery
 
-});
+// });
