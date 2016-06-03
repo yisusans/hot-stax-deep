44788b2f3ea482b700252021695f13e52a5e36fb
Chris Praktish & parker smathers & Susan Yi <pair+cprakti.psmathers.yisusans@devbootcamp.com>
2016-06-03 16:33:58 -0400

    fix merging conflicts, vote button, add answers work

--- public/js/application.js
+++ public/js/application.js
@@ -7,7 +7,6 @@ $(document).ready( function(){
       method: "GET",
       url: $data.questionId + '/answers'
     });
-
     request.done(function (msg){
       $("#answer-form-show").html(msg)
     });
@@ -34,6 +33,7 @@ $(document).ready( function(){
     });
   });
 
+
   $(".question-box").on ("submit", function(event){
     event.preventDefault();
     var $target = $(event.target)
@@ -50,6 +50,8 @@ $(document).ready( function(){
   });
 
 
+
+
   $(".answers-post").on ("submit", ".vote-form", function(event){
     event.preventDefault();
     var $target = $(event.target)
@@ -60,6 +62,7 @@ $(document).ready( function(){
       data: $target.serialize()
     });
     request.done(function (msg) {
+      // debugger
       $target.parent().find('.vote-count').text(msg["votes"])
     });
 
@@ -67,98 +70,100 @@ $(document).ready( function(){
       console.log(msg)
     });
   });
-
-  $("#question-get-comment").on ("click", function(event){
-    event.preventDefault();
-    var $target = $(event.target);
-    var $data = $target.data();
-    var request = $.ajax({
-      method: "GET",
-      url:$data.questionId + '/comments',
-      data: $target.serialize()
-    });
-
-    request.done(function (msg) {
-      $("#question-comment-form-show").html(msg);
-    });
-    request.fail(function (msg) {
-      console.log(msg)
-    });
-  });
-
-  $("#question-comments-show").on ("submit", "#q-comment-form", function(event) {
-    event.preventDefault();
-    var $target = $(event.target);
-    var $data = $target.data();
-
-    var request = $.ajax({
-      method: "POST",
-      url: $data.questionId + '/comments',
-      data: $target.serialize()
-    });
-
-    request.done(function (msg) {
-      $("#question-comments-show").append(msg);
-      $target.remove();
-    });
-    request.fail(function (msg) {
-      console.log(msg)
-    });
-  });
-
-  $("#answer-get-comment").on ("click", function(event) {
-    event.preventDefault();
-    var $target = $(event.target);
-    var $data = $target.data();
-
-    var request = $.ajax({
-      method: "GET",
-      url:'/answers/' + $data.answerId + '/comments',
-      data: $target.serialize()
-    });
-    request.done(function (msg) {
-      $("#answer-comment-form-show").html(msg);
-    });
-  });
-
-$(".question_show").on ("submit", ".vote-form-question", function(event){
-    event.preventDefault();
-    var $target = $(event.target)
-    var $data = $target.data()
-    debugger;
-    var request = $.ajax ({
-      method: "POST",
-      url: '/questions/'+ $data.questionId + '/votes',
-      data: $target.serialize()
-    });
-
-    request.done(function (msg) {
-      $target.parent().find('.vote-count').text(msg["votes"])
-    });
-
-    request.fail(function (msg) {
-      console.log(msg)
-    });
-)};
-
-  $("#answer-comment-form-show").on ("submit", function(event) {
-    event.preventDefault();
-    var $target = $(event.target);
-    var $data = $target.data();
-
-    var request = $.ajax({
-      method: "POST",
-      url: '/answers/' + $data.answerID + '/comments',
-      data: $target.serialize()
-    });
-    request.done(function (msg) {
-      $("#comments-show").append(msg);
-      $target.remove();
-    });
-  });
-
 
 });
+
+
+//   $("#question-get-comment").on ("click", function(event){
+//     event.preventDefault();
+//     var $target = $(event.target);
+//     var $data = $target.data();
+//     var request = $.ajax({
+//       method: "GET",
+//       url:$data.questionId + '/comments',
+//       data: $target.serialize()
+//     });
+
+//     request.done(function (msg) {
+//       $("#question-comment-form-show").html(msg);
+//     });
+//     request.fail(function (msg) {
+//       console.log(msg)
+//     });
+//   });
+
+//   $("#question-comments-show").on ("submit", "#q-comment-form", function(event) {
+//     event.preventDefault();
+//     var $target = $(event.target);
+//     var $data = $target.data();
+
+//     var request = $.ajax({
+//       method: "POST",
+//       url: $data.questionId + '/comments',
+//       data: $target.serialize()
+//     });
+
+//     request.done(function (msg) {
+//       $("#question-comments-show").append(msg);
+//       $target.remove();
+//     });
+//     request.fail(function (msg) {
+//       console.log(msg)
+//     });
+//   });
+
+//   $("#answer-get-comment").on ("click", function(event) {
+//     event.preventDefault();
+//     var $target = $(event.target);
+//     var $data = $target.data();
+
+//     var request = $.ajax({
+//       method: "GET",
+//       url:'/answers/' + $data.answerId + '/comments',
+//       data: $target.serialize()
+//     });
+//     request.done(function (msg) {
+//       $("#answer-comment-form-show").html(msg);
+//     });
+//   });
+
+// $(".question_show").on ("submit", ".vote-form-question", function(event){
+//     event.preventDefault();
+//     var $target = $(event.target)
+//     var $data = $target.data()
+//     debugger;
+//     var request = $.ajax ({
+//       method: "POST",
+//       url: '/questions/'+ $data.questionId + '/votes',
+//       data: $target.serialize()
+//     });
+
+//     request.done(function (msg) {
+//       $target.parent().find('.vote-count').text(msg["votes"])
+//     });
+
+//     request.fail(function (msg) {
+//       console.log(msg)
+//     });
+// )};
+
+//   $("#answer-comment-form-show").on ("submit", function(event) {
+//     event.preventDefault();
+//     var $target = $(event.target);
+//     var $data = $target.data();
+
+//     var request = $.ajax({
+//       method: "POST",
+//       url: '/answers/' + $data.answerID + '/comments',
+//       data: $target.serialize()
+//     });
+//     request.done(function (msg) {
+//       $("#comments-show").append(msg);
+//       $target.remove();
+//     });
+//   });
+
+// });
 
 
 
